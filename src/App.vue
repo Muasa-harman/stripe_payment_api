c05259f01813601af1b049114153798c0b72cb4f
Muasa-harman <harman.muasa@gmail.com>
2024-09-08 21:26:23 +0300

    apis in typescript

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="app" :class="$style['app']">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import styles from "./App.module.css";
+
+export default {
+  name: "App",
+  computed: {
+    $style() {
+      return styles;
+    },
+  },
+};
+</script>
---
+++ stripe_api_backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const stripe = require('stripe')('YOUR_SECRET_STRIPE_KEY');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/create-checkout-session', async (req, res) => {
+  const { donation } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'Donation',
+        },
+        unit_amount: donation * 100,
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: '<http://localhost:5001/success>',
+    cancel_url: '<http://localhost:5001/error>',
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(3000, () => console.log('Backend server running on port 3000'));
